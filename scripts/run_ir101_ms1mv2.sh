98688af48955a191ac1e2df246b5267f3666b5e1
Gefei Shen <35111787+GefeiSHEN@users.noreply.github.com>
2023-11-18 13:58:21 -0600

    feat: image resize script and finetuning

--- scripts/run_ir101_ms1mv2.sh
+++ scripts/run_ir101_ms1mv2.sh
@@ -1,15 +1,13 @@
 
-python main.py \
-    --data_root /mnt/home/kimminc2/data \
-    --train_data_path faces_emore \
-    --val_data_path faces_emore \
-    --prefix ir101_ms1mv2_adaface \
-    --use_wandb \
-    --use_mxrecord \
-    --gpus 2 \
+python3 main.py \
+    --data_root ./train_dataset/ \
+    --train_data_path adaface_images \
+    --val_data_path adaface_images \
+    --prefix ir101_ms1mv2_adaface_finetune \
+    --gpus 1 \
     --use_16bit \
     --arch ir_101 \
-    --batch_size 512 \
+    --batch_size 200 \
     --num_workers 16 \
     --epochs 26 \
     --lr_milestones 12,20,24 \
@@ -19,5 +17,7 @@ python main.py \
     --h 0.333 \
     --low_res_augmentation_prob 0.2 \
     --crop_augmentation_prob 0.2 \
-    --photometric_augmentation_prob 0.2
+    --photometric_augmentation_prob 0.2 \
+    --resume_from_checkpoint ./pretrained/adaface_ir101_ms1mv2.ckpt \
+    --custom_num_class 2370	
 
