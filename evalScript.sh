c8c7507afa06f2b043ff64c41c4ac68a85718841
Gefei Shen <35111787+GefeiSHEN@users.noreply.github.com>
2023-12-10 06:54:25 -0600

    feat: evaluation script

--- evalScript.sh
+++ evalScript.sh
@@ -1,11 +1,11 @@
-python main.py \
-    --data_root data \
+python3 main.py \
+    --data_root train_dataset \
     --train_data_path dogFace_112 \
     --val_data_path dogFace_112 \
     --prefix adaface_ir101_ms1mv2 \
     --gpus 1 \
     --use_16bit \
-    --start_from_model_statedict ./pretrained/adaface_ir101_ms1mv2.ckpt \
+    --start_from_model_statedict ./epoch=32-step=4191.ckpt \
     --arch ir_101 \
     --evaluate \
     --use_mxrecord \
